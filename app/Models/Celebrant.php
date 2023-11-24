2110dfcfac1275a3716c5e95ce5ab4c1f6a9d91f
stefano-mazziotta <mazziottastefano@gmail.com>
2023-11-24 00:21:02 -0300

    create models

---
+++ app/Models/Celebrant.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Celebrant extends Model
+{
+    use HasFactory;
+
+    protected $table = "celebrants";
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'birthday',
+        'phone',
+        'relationship_id'
+    ];
+}
