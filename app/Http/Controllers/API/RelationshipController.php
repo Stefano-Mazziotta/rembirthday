afcb25abb43ea84f0e325112d85899a555f062db
stefano-mazziotta <mazziottastefano@gmail.com>
2023-11-24 00:21:09 -0300

    create controllers

---
+++ app/Http/Controllers/API/RelationshipController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Relationship;
+use Illuminate\Http\Request;
+
+class RelationshipController extends Controller
+{
+    public function getAll(){
+        $data = Relationship::all();
+        return response()->json($data, 200);
+    }
+
+    public function create(Request $request){
+        $data['name'] = $request['name'];
+        Relationship::create();
+        return response()->json([
+            'message' => "Successfully created",
+            'success' => true
+        ], 200);
+    }
+}
