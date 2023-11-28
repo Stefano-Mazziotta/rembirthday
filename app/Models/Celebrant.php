3c275b8701340381bfcc6323d531f900b407ad0f
stefano-mazziotta <mazziottastefano@gmail.com>
2023-11-27 22:27:42 -0300

    Celebrant - Model - access to relationship

--- app/Models/Celebrant.php
+++ app/Models/Celebrant.php
@@ -10,7 +10,7 @@ class Celebrant extends Model
     use HasFactory;
 
     protected $table = "celebrants";
-
+    
     protected $fillable = [
         'name',
         'surname',
@@ -19,4 +19,8 @@ class Celebrant extends Model
         'phone',
         'relationship_id'
     ];
+
+    public function relationship(){
+        return $this->belongsTo(Relationship::class, 'relationship_id');
+    }
 }
